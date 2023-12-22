c929a4cc140db288b45d4d3f5ba757db034ed09f
Pascal Bürkle <25956751+pabueco@users.noreply.github.com>
2023-12-22 22:43:57 +0100

    setup nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxt/ui"],
+
+  ui: {
+    icons: ["tabler"],
+  },
+  // (optional) Enable the Nuxt devtools
+  devtools: { enabled: true },
+  // Enable SSG
+  ssr: false,
+  vite: {
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    // Enable environment variables
+    // Additional environment variables can be found at
+    // https://tauri.app/2/reference/environment-variables/
+    envPrefix: ["VITE_", "TAURI_"],
+    server: {
+      // Tauri requires a consistent port
+      strictPort: true,
+
+      hmr: {
+        // Use websocket for mobile hot reloading
+        protocol: "ws",
+        // Make sure it's available on the network
+        host: "0.0.0.0",
+        // Use a specific port for hmr
+        port: 5183,
+      },
+    },
+  },
+  srcDir: "src",
+});
